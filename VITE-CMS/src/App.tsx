387b38f3d9791d5ff105bfee2fc84289c1b598bd
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2024-11-12 11:14:12 +0100

    refactoring

--- VITE-CMS/src/App.tsx
+++ VITE-CMS/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
-import Card from './components/Card'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-
   return (
     <>
-    <h1 className='font-bold'>Super</h1>
-   <Card></Card>
-   </>
-  )
+      <h1 className="font-bold">Super</h1>
+      <Card></Card>
+    </>
+  );
 }
 
-export default App
+export default App;
