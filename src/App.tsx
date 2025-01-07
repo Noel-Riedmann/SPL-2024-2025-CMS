918a31c7788139991eb44504c08730e2ff9afb3f
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2025-01-07 11:21:12 +0100

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+// App.tsx
 import "./App.css";
 import CardManager from "./components/CardManager";
+import Homework from "./components/Homework";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-     <CardManager></CardManager>
-    </>
+    <Router>
+      <nav>
+        <Link to="/">Card Manager</Link> | <Link to="/homework">Homework</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<CardManager />} />
+        <Route path="/homework" element={<Homework />} />
+      </Routes>
+    </Router>
   );
 }
 
